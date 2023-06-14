4547191b6c2a3251a290a369ab2f65800333be2f
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-06-14 10:50:11 -0300

    feat: add sobremim

--- src/paginas/sobreMim/index.js
+++ src/paginas/sobreMim/index.js
@@ -1,6 +1,23 @@
+import PostModelo from "../../componentes/PostModelo";
+import fotoCapa from "../../assets/sobre_mim_capa.png"
+import styles from "./SobreMim.module.css";
+
 const SobreMim = () => {
     return(
-            <h1>Sobre Mim</h1>
+            <PostModelo 
+                fotoCapa={fotoCapa}
+                titulo="Sobre mim"
+            >
+            <h3 className={styles.subtitulo}>
+                Olá, eu sou Celso
+            </h3>
+            <img 
+                src="https://github.com/celsoTaliatelli.png" 
+                alt="Foto de Celso"
+                className={styles.fotoSobreMim}
+                />
+            <p className={styles.paragrafo}>...</p>
+            </PostModelo>
     );
 }
 
